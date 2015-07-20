3f53e6b2a2ce7f68a5a4519d3f207796616fab54
Sam Cooper <sam.cooper@scisys.co.uk>
2015-07-20 12:54:51 +0100

    Added in helper method for registering service providers in the directory service.

--- CCSDS_COMMON_SUPPORT/src/main/java/esa/mo/common/support/BaseCommonServer.java
+++ CCSDS_COMMON_SUPPORT/src/main/java/esa/mo/common/support/BaseCommonServer.java
@@ -21,16 +21,20 @@
 package esa.mo.common.support;
 
 import esa.mo.com.support.BaseComServer;
+import java.util.Arrays;
 import org.ccsds.moims.mo.common.CommonHelper;
 import org.ccsds.moims.mo.mal.MALContext;
 import org.ccsds.moims.mo.mal.MALContextFactory;
 import org.ccsds.moims.mo.mal.MALElementFactoryRegistry;
 import org.ccsds.moims.mo.mal.MALException;
 import org.ccsds.moims.mo.mal.MALInteractionException;
+import org.ccsds.moims.mo.mal.MALService;
 import org.ccsds.moims.mo.mal.consumer.MALConsumerManager;
+import org.ccsds.moims.mo.mal.provider.MALInteractionHandler;
 import org.ccsds.moims.mo.mal.provider.MALProviderManager;
 import org.ccsds.moims.mo.mal.structures.Identifier;
 import org.ccsds.moims.mo.mal.structures.IdentifierList;
+import org.ccsds.moims.mo.mal.structures.IntegerList;
 import org.ccsds.moims.mo.mal.structures.URI;
 
 /**
@@ -38,18 +42,21 @@ import org.ccsds.moims.mo.mal.structures.URI;
  */
 public abstract class BaseCommonServer extends BaseComServer
 {
+  private final Identifier providerName;
   protected final DirectoryServiceWrapper directoryService;
 
   /**
    * Constructor.
    *
+   * @param providerName The name of this provider to be used in the directory service.
    * @param domain the domain of the service provider.
    * @param network The network of the service provider.
    */
-  public BaseCommonServer(IdentifierList domain, Identifier network)
+  public BaseCommonServer(final String providerName, final IdentifierList domain, final Identifier network)
   {
     super(domain, network);
 
+    this.providerName = new Identifier(providerName);
     directoryService = new DirectoryServiceWrapper();
   }
 
@@ -60,13 +67,21 @@ public abstract class BaseCommonServer extends BaseComServer
    * @param mal The MAL context to use.
    * @param consumerMgr The consumer manager to use.
    * @param providerMgr The provider manager to use.
+   * @param providerName The name of this provider to be used in the directory service.
    * @param domain the domain of the service provider.
    * @param network The network of the service provider.
    */
-  public BaseCommonServer(MALContextFactory malFactory, MALContext mal, MALConsumerManager consumerMgr, MALProviderManager providerMgr, IdentifierList domain, Identifier network)
+  public BaseCommonServer(final MALContextFactory malFactory,
+          final MALContext mal,
+          final MALConsumerManager consumerMgr,
+          final MALProviderManager providerMgr,
+          final String providerName,
+          final IdentifierList domain,
+          final Identifier network)
   {
     super(malFactory, mal, consumerMgr, providerMgr, domain, network);
 
+    this.providerName = new Identifier(providerName);
     directoryService = new DirectoryServiceWrapper();
   }
 
@@ -85,5 +100,31 @@ public abstract class BaseCommonServer extends BaseComServer
 
     String duri = System.getProperty("directory.uri", "rmi://localhost:1024/1024-DirectoryService");
     directoryService.init(consumerMgr, new URI(duri));
+  }
+
+  /**
+   * Helper method that creates the MAL provider instance and registers it in the Directory service.
+   *
+   * @param <T> The service provider type.
+   * @param service The MAL service being provided.
+   * @param supportCapabilities The supported capability numbers being provided.
+   * @param handler The service provider implementation.
+   * @param isPublisher True if service provider is a publisher.
+   * @return The supplied handler object.
+   * @throws MALException On error.
+   * @throws MALInteractionException On error.
+   */
+  protected <T extends MALInteractionHandler> T createAndPublishProvider(MALService service,
+          Integer[] supportCapabilities,
+          T handler,
+          boolean isPublisher) throws MALException, MALInteractionException
+  {
+    IntegerList scl = new IntegerList();
+    scl.addAll(Arrays.asList(supportCapabilities));
+
+    createProvider(service, handler, isPublisher);
+    directoryService.publishProvider(providerName, domain, network, service, scl, ep.getURI(), ep.getURI());
+
+    return handler;
   }
 }
